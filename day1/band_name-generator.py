f77ae9c790179bc9cc5bd193bb6f54301942d873
Domuh <hadiahameed53@gmail.com>
2025-08-21 23:07:43 +0500

    day1

---
+++ day1/band_name-generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator... \n")
+city_name = input("What's the name of city in which you grew up: \n")
+pet_name = input("What's your pet name: \n")
+print("Your band name could be: " + city_name +" "+ pet_name)
+
+      
+
--- day1/task.py
+++ day1/task.py
@@ -1,2 +1,28 @@
+#1--------- print and input command-----------
+
 #print("Hello world ")
-print("Hello" + " " + input("Whats your name? ") + "!")
+#print("Hello" + " " + input("Whats your name? ") + "!")  
+
+#2-----------variables---------
+
+#name = "jack"
+#print(name)
+
+#name = input("Enter your name: ")
+#print (name)
+
+#3----- len()  shows the length of string , str () covert int into string, f-string (formatted string literals)
+
+#name = input("Enter your name: ")
+#length = len(name)
+#print(length)
+#print("Your name is " + name + ",and the lenght of the character is: " + str(length))     # con the int with string
+#print(f"Your name is {name},and the lenght of the character is: {length}")   # Using f-strings (Python 3.6+)
+
+#4-------- swaping varable values 
+
+#glass1 = "milk"
+#glass2 = "juice"
+#glass1,glass2 = glass2,glass1
+#
+#print(glass1,glass2)
